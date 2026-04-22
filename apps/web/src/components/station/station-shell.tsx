b12b8c08f5f7fbbd462c1abe8581c6f63a640ea8
armanghev <armanghevondyan2008@gmail.com>
2026-04-21 22:11:25 -0700

    refactor: remove unused inspector panel

--- apps/web/src/components/station/station-shell.tsx
+++ apps/web/src/components/station/station-shell.tsx
@@ -1,26 +1,20 @@
 import { AppSidebar } from "@/components/sidebar-01/app-sidebar";
-import { InspectorDrawer } from "@/components/station/inspector-drawer";
-import { StationInspector } from "@/components/station/station-inspector";
 import { StationTopbar } from "@/components/station/station-topbar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 type StationShellProps = {
   children: React.ReactNode;
-  inspector?: React.ReactNode;
 };
 
-export function StationShell({ children, inspector }: StationShellProps) {
+export function StationShell({ children }: StationShellProps) {
   return (
     <SidebarProvider>
       <div className="relative flex min-h-screen w-full bg-background">
         <AppSidebar />
         <SidebarInset className="flex min-w-0 flex-1 flex-col bg-background">
-          <div className="grid min-h-screen xl:grid-cols-[minmax(0,1fr)_20rem]">
-            <div className="min-w-0">
-              <StationTopbar />
-              <main className="p-4 lg:p-6">{children}</main>
-            </div>
-            <InspectorDrawer>{inspector ?? <StationInspector />}</InspectorDrawer>
+          <div className="min-h-screen min-w-0">
+            <StationTopbar />
+            <main className="p-4 lg:p-6">{children}</main>
           </div>
         </SidebarInset>
       </div>
